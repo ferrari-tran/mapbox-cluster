262b8164fbc253c41c7095f772169414fcc3305d
Lao Ton <laoton@MacPro-M1-Lao-Ton.local>
2022-07-21 21:01:25 +0700

    Setup and trying with mapbox cluster

--- src/App.js
+++ src/App.js
@@ -1,23 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "./App.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWl6dW5la284NiIsImEiOiJjazYxbmhzcXowNWoyM21zY2lpemRtMTJwIn0.zNhaJaXle_1Yssue-lXhNQ";
+
+// const locations = [
+//   {poi_id: 1, name: 'EQ Works', lat: 43.6708, lon: -79.3899},
+//   {poi_id: 2, name: 'CN Tower', lat: 43.6426, lon: -79.3871},
+//   {poi_id: 3, name: 'Niagara Falls', lat: 43.0896, lon: -79.0849},
+//   {poi_id: 4, name: 'Vancouver Harbour', lat: 49.2965, lon: -123.0884}
+// ]
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+
+    map.current.on("load", () => {
+      
+        map.current.addSource("data", {
+          type: "geojson",
+          // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+          // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+          data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
+          cluster: true,
+          clusterMaxZoom: 14, // Max zoom to cluster points on
+          clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+        });
+
+        map.current.addLayer({
+          id: "clusters",
+          type: "circle",
+          source: "data",
+          filter: ["has", "point_count"],
+          paint: {
+            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            "circle-color": [
+              "step",
+              ["get", "point_count"],
+              "#51bbd6",
+              100,
+              "#f1f075",
+              750,
+              "#f28cb1",
+            ],
+            "circle-radius": [
+              "step",
+              ["get", "point_count"],
+              20,
+              100,
+              30,
+              750,
+              40,
+            ],
+          },
+        });
+
+        map.current.addLayer({
+          id: "cluster-count",
+          type: "symbol",
+          source: "data",
+          filter: ["has", "point_count"],
+          layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+            "text-size": 12,
+          },
+        });
+
+        map.current.addLayer({
+          id: "unclustered-point",
+          type: "circle",
+          source: "data",
+          filter: ["!", ["has", "point_count"]],
+          paint: {
+            "circle-color": "#11b4da",
+            "circle-radius": 4,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff",
+          },
+        });
+      
+    });
+
+    // const mapClick = useCallback(() => {
+    map.current.on("click", "clusters", (e) => {
+      const features = map.current.queryRenderedFeatures(e.point, {
+        layers: ["clusters"],
+      });
+      const clusterId = features[0].properties.cluster_id;
+      map.current
+        .getSource("data")
+        .getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return;
+
+          map.current.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom,
+          });
+        });
+    });
+    // }, []);
+
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.current.on("click", "unclustered-point", (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const mag = e.features[0].properties.mag;
+      const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
+
+      // Ensure that if the map is zoomed out such that
+      // multiple copies of the feature are visible, the
+      // popup appears over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
+        .addTo(map);
+    });
+
+    map.current.on("mouseenter", "clusters", () => {
+      map.current.getCanvas().style.cursor = "pointer";
+    });
+    map.current.on("mouseleave", "clusters", () => {
+      map.current.getCanvas().style.cursor = "";
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
---
+++ src/mapbox/Mapbox.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import ReactMapboxGl from "react-mapbox-gl";
+import {ReactMapboxGlCluster} from 'react-mapbox-gl-cluster';
+
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1IjoibWl6dW5la284NiIsImEiOiJjazYxbmhzcXowNWoyM21zY2lpemRtMTJwIn0.zNhaJaXle_1Yssue-lXhNQ',
+});
+
+const mapProps = {
+  center: [-95.7129, 37.0902],
+  zoom: [3],
+  style: "mapbox://styles/mapbox/streets-v8",
+};
+
+const CustomSpiralComponent = ({ properties, ...restProps }) => {
+  const onClick = (e) => {
+    console.log(
+      `Receive event onClick in spiral at properties: ${JSON.stringify(
+        properties
+      )}`
+    );
+  };
+  return <div className="spiderifier-marker-content" onClick={onClick}></div>;
+};
+
+const CustomeMarkerComponent = ({ properties, className, cssObject }) => {
+  const onClick = (e) => {
+    console.log(
+      `Receive event onClick in marker at properties: ${JSON.stringify(
+        properties
+      )}`
+    );
+  };
+  return <div className={className} style={cssObject} onClick={onClick} />;
+};
+
+class Mapbox extends Component {
+  getEventHandlers() {
+    return {
+      onClick: (properties, coords, offset) =>
+        console.log(
+          `Receive event onClick at properties: ${properties}, coords: ${coords}, offset: ${offset}`
+        ),
+      onMouseEnter: (properties, coords, offset) =>
+        console.log(
+          `Receive event onMouseEnter at properties: ${properties}, coords: ${coords}, offset: ${offset}`
+        ),
+      onMouseLeave: (properties, coords, offset) =>
+        console.log(
+          `Receive event onMouseLeave at properties: ${properties}, coords: ${coords}, offset: ${offset}`
+        ),
+      onClusterClick: (properties, coords, offset) =>
+        console.log(
+          `Receive event onClusterClick at properties: ${properties}, coords: ${coords}, offset: ${offset}`
+        ),
+      onClusterMouseEnter: (properties, coords, offset) =>
+        console.log(
+          `Receive event onClusterMouseEnter at properties: ${properties}, coords: ${coords}, offset: ${offset}`
+        ),
+      onClusterMouseLeave: (properties, coords, offset) =>
+        console.log(
+          `Receive event onClusterMouseLeave at properties: ${properties}, coords: ${coords}, offset: ${offset}`
+        ),
+    };
+  }
+
+  render() {
+    return (
+      <div className="Mapbox">
+        <Map {...mapProps} onStyleLoad={this.onStyleLoad}>
+          <ReactMapboxGlCluster
+            data={this.props.data}
+            {...this.getEventHandlers()}
+            spiralComponent={CustomSpiralComponent}
+            markerComponent={CustomeMarkerComponent}
+            clusterClickEnabled={true}
+          />
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default Mapbox;
